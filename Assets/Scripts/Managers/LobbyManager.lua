289430dd0cb0a4a0a1e68e7584a0c579055d611e
Noscarl <101284470+Noscarl@users.noreply.github.com>
2024-05-13 20:37:01 +0800

    small changes on ui. [STILL WIP]

--- Assets/Scripts/Managers/LobbyManager.lua
+++ Assets/Scripts/Managers/LobbyManager.lua
@@ -23,8 +23,6 @@ function self:ClientAwake()
     setLobbyCount:Connect(function(arg) 
         GameManager.LobbyDataModule.setPlayerCount(arg)
     end)
-     
-
 end
 
 function onStartContest(playerCount)
@@ -48,65 +46,11 @@ function onPlayerJoined(player)
     checkLobbyContest()
     checkStartContest()
 end
-
-
-function onPlayerLeft(player)--!Type(ClientAndServer)
-local GameManager = require("GameModuleManager")
-
-local lobbyPlayerCountRequest = Event.new("LobbyPlayerCountRequest")
-local startContest = Event.new("StartContest")
-
-local setPlayerSetting = Event.new("SetPlayerSetting")
-local setLobbyCount = Event.new("SetLobbyCount")
-
-local lobbyQueue = {}
-local minPlayers = 3
-
--- Client
-function self:ClientAwake()
-    
-    lobbyPlayerCountRequest:FireServer(#lobbyQueue)
-    startContest:Connect(function(arg) 
-        onStartContest(arg)
-    end)
-    setPlayerSetting:Connect(function(arg1, arg2) 
-        onLocalPlayerData(arg1)
-    end)
-    setLobbyCount:Connect(function(arg) 
-        GameManager.LobbyDataModule.setPlayerCount(arg)
-    end)
-
-end
-
-function onStartContest(playerCount)
-    print("Contest starting with " .. playerCount .. " players.")
-end
-function onLocalPlayerData(thisPlayer)
-    if (thisPlayer == client.localPlayer) then
-        -- print("local player: " .. thisPlayer.name)
-        GameManager.PlayerDataModule.setPlayerData(thisPlayer.id, thisPlayer.name)
-    end
-end
-
--- Server
-function self:ServerAwake()
-    server.PlayerConnected:Connect(onPlayerJoined)
-    server.PlayerDisconnected:Connect(onPlayerLeft)
-
-end
-function onPlayerJoined(player)
-    table.insert(lobbyQueue, player)
-    print(player.name .. " Joined " .. #lobbyQueue .. " players.")
-    checkLobbyContest()
-    checkStartContest()
-end
-
 
 function onPlayerLeft(player)
     for i, p in ipairs(lobbyQueue) do
         if p == player then
             table.remove(lobbyQueue, i)
-            GameManager.LobbyDataModule.players[player] = nil
             print(player.name .. " Left " .. #lobbyQueue .. " players.")
             updateLobbyContest()
             break
@@ -130,29 +74,15 @@ function updateLobbyContest()
         print("Update Client")
 end
 
-    for i, p in ipairs(lobbyQueue) do
-        if p == player then
-            table.remove(lobbyQueue, i)
-            GameManager.LobbyDataModule.players[player] = nil
-            print(player.name .. " Left " .. #lobbyQueue .. " players.")
-            updateLobbyContest()
-            break
-        end
-    end
-end
+-- function checkCharacter(player)
+--     player.CharacterChanged:Connect(function(player, character) 
+--         local playerinfo = players[player] -- After the player's character is instantiated store their info from the player table (`player`,`score`)
+--         if (character == nil) then
+--             return --If no character instantiated return
+--         end 
 
-function checkStartContest()
-    if #lobbyQueue >= minPlayers then
-        startContest:FireAllClients(#lobbyQueue)
-    end
-end
-function checkLobbyContest()
-    lobbyPlayerCountRequest:Connect(function(player, arg)
-        setPlayerSetting:FireAllClients(player)
-        setLobbyCount:FireAllClients(#lobbyQueue)
-    end)
-end
-function updateLobbyContest()
-        setLobbyCount:FireAllClients(#lobbyQueue)
-        print("Update Client")
-end
+--         if characterCallback then -- If there is a character callback provided call it with a reference to the player info
+--             characterCallback(playerinfo)
+--         end
+--     end)
+-- end
